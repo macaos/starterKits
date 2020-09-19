461f96fdf2e65c3a6a9caa437e05b2f6fa33bbdf
macaos <help@kareui.com>
2020-09-19 10:59:52 +0900

    페이지 인덱스 뒤로 넘어가는 버그 수정

--- react_insertview/src/views/Route.tsx
+++ react_insertview/src/views/Route.tsx
@@ -9,48 +9,49 @@ import Page02 from './Page02';
 import Page03 from './Page03';
 import Page04 from './Page04';
 
-class Route extends Component<{},{
-    showPages:string[]
+class Route extends Component<{}, {
+    showPages: string[]
 }> {
     state = {
-        showPages:['Home']
+        showPages: ['Home']
     }
     screenWidth = (window as any).screen.width;
-    constructor(props:{}){
+    constructor(props: {}) {
         super(props);
         console.log('이건 한번?Route');
-        EventEmitter.subscribe(Event.INSERT_PAGE,(options:any)=>{
-            console.log('showPagess',this.state.showPages);
-            let newArr:string[] = JSON.parse(JSON.stringify(this.state.showPages));//showPages => [...showPages];
+        EventEmitter.subscribe(Event.INSERT_PAGE, (options: any) => {
+            console.log('showPagess', this.state.showPages);
+            let newArr: string[] = JSON.parse(JSON.stringify(this.state.showPages));//showPages => [...showPages];
             newArr.push(options.pageID as string);
-            console.log('newArr',newArr)
+            console.log('newArr', newArr)
             // setShowPages(newArr)
             this.setState({
                 ...this.state,
                 showPages: newArr,
             })
         })
-        EventEmitter.subscribe(Event.GOTO_BACK,(options:any)=>{
-            console.log('showPagessd',this.state.showPages);
+        EventEmitter.subscribe(Event.GOTO_BACK, (options: any) => {
+            console.log('showPagessd', this.state.showPages);
             // return;
-            let newArr:string[] = this.state.showPages.slice(0, this.state.showPages.length+options.idx);
-            console.log('newArr',newArr)
+            let newArr: string[] = this.state.showPages.slice(0, this.state.showPages.length + options.idx);
+            if (newArr.length === 0) return;
+            console.log('newArr', newArr)
             this.setState({
                 ...this.state,
                 showPages: newArr,
             })
         })
-        EventEmitter.subscribe(Event.RESET_PAGE,(options:any)=>{
-            let newArr:string[] = options.arr;
-            console.log('newArr',newArr)
+        EventEmitter.subscribe(Event.RESET_PAGE, (options: any) => {
+            let newArr: string[] = options.arr;
+            console.log('newArr', newArr)
             this.setState({
                 ...this.state,
                 showPages: newArr,
             })
         })
-        EventEmitter.subscribe(Event.RESET_PAGE,(options:any)=>{
-            let newArr:string[] = options.arr;
-            console.log('newArr',newArr)
+        EventEmitter.subscribe(Event.RESET_PAGE, (options: any) => {
+            let newArr: string[] = options.arr;
+            console.log('newArr', newArr)
             this.setState({
                 ...this.state,
                 showPages: newArr,
@@ -58,23 +59,23 @@ class Route extends Component<{},{
         })
     }
     render() {
-        const {showPages} = this.state;
-        const Pages = showPages.map((name:string)=>{
-            switch(name) {
+        const { showPages } = this.state;
+        const Pages = showPages.map((name: string) => {
+            switch (name) {
                 case 'Home':
-                    return <Home key="Home"/>;
+                    return <Home key="Home" />;
                     break;
                 case 'Page01':
-                    return <Page01 key="Page01"/>;
+                    return <Page01 key="Page01" />;
                     break;
                 case 'Page02':
-                    return <Page02 key="Page02"/>;
+                    return <Page02 key="Page02" />;
                     break;
                 case 'Page03':
-                    return <Page03 key="Page03"/>;
+                    return <Page03 key="Page03" />;
                     break;
                 case 'Page04':
-                    return <Page04 key="Page04"/>;
+                    return <Page04 key="Page04" />;
                     break;
             }
             return
@@ -82,9 +83,9 @@ class Route extends Component<{},{
         return (
             <div>
                 <Alert />
-                <div className={classnames(["page-container",css`
-                    width: ${this.screenWidth*showPages.length+50}px;
-                    margin-left: ${this.screenWidth*(showPages.length-1)*-1}px;
+                <div className={classnames(["page-container", css`
+                    width: ${this.screenWidth * showPages.length + 50}px;
+                    margin-left: ${this.screenWidth * (showPages.length - 1) * -1}px;
                 `])}>
 
                     {Pages}
